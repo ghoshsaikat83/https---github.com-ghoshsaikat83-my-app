8737522c2c2b987229196b14383c3160bf1f4e1d
saikat.ghosh <saikat.ghosh@NMG-D-83>
2024-02-26 19:14:09 +0530

    FirstReactApp

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Header from './Header';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function Footer() {
+  return (
+      <footer>
+          <small>© 2024 Saikat development. All rights reserved.</small>
+      </footer>
+  )
+}
+
+function MainContent() {
+  return (
+      <div>
+          <h1>Reasons I'm excited to learn React</h1>
+          <ol>
+              <li>It's a popular library, so I'll be 
+              able to fit in with the cool kids!</li>
+              <li>I'm more likely to get a job as a developer
+              if I know React</li>
+          </ol>
+      </div>
+  )
+}
+
+function Page() {
+  return (
+      <div>
+          <Header />
+          <MainContent />
+          <Footer />
+      </div>
+  )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
+
